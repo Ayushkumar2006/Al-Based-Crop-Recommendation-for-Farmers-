1cc935caf13e8dfbb598cffebbcedf1c3d114433
Ayush Kumar <ayushkumardth2006@gmail.com>
2026-07-19 14:00:41 +0530

    Enable debug

--- Crop_Recommendation/settings.py
+++ Crop_Recommendation/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9lp2i9mt@h_w&*%$-=kubo_sbpc0#1n$(woqd(s)c7gd-bl!7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "al-based-crop-recommendation-for-farmers-production.up.railway.app",
